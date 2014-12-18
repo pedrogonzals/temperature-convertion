02386b5174d62a60a2fe9833f906c35025038b22
pedro gonzales <pedrogonzals@gmail.com>
2014-12-18 11:49:48 -0800

    python conversion program
    a small program that converts fahrenheit to celsius

---
+++ input.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 10, 2014
+
+@author: PedroG
+'''
+
+print " to this program converts fahrenheit to celsius"
+print " type in a temperature in fahrenheit:",
+fahrenheit = float(raw_input())
+celsius =(fahrenheit - 32)*5.0 /9
+print "that is",
+print celsius,
+print " degrees celsius"
